7855b0765984ad277430154088d73f891de130fd
saikat mondal <62213201+codingbysaikat@users.noreply.github.com>
2022-11-08 19:25:16 +0600

    up to date

--- maps.php
+++ maps.php
@@ -17,10 +17,11 @@ register_activation_hook(__FILE__,"wordcount_activation_hook");
 function wordcount_deactivation_hook(){}
 register_deactivation_hook(__FILE__,"wordcount_deactivation_hook");*/
 
-function gmaps_loaded_textdomin(){
-    load_plugin_textdomain( 'gmaps', false, dirname(__FILE__ . 'languages') );
+function gmaps_loaded_textdomin()
+{
+    load_plugin_textdomain('gmaps', false, dirname(__FILE__ . 'languages'));
 }
-add_action( 'plugin_loaded', 'gmaps_loaded_textdomin' );
+add_action('plugin_loaded', 'gmaps_loaded_textdomin');
 
 
 
@@ -31,21 +32,30 @@ add_action( 'plugin_loaded', 'gmaps_loaded_textdomin' );
 
 
 
-function gmaps_display(){
+function gmaps_display($attr)
+{
     $defults = array(
-        'place'=>'khulna',
+        'place' => 'khulna',
+        'zoom' => '14',
+        'width' => '800',
+        'height' => '500',
 
 
     );
-    $pram = shortcode_atts();
+    $params = shortcode_atts($defults, $attr);
+    $map = <<<EOD
+    <div>
+        <div>
+            <iframe width="{$params['width']}" height="{$params['height']}"
+                    src="https://maps.google.com/maps?q={$params['place']}&t=&z={$params['zoom']}&ie=UTF8&iwloc=&output=embed"
+                    frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+            </iframe>
+        </div>
+    </div>
+    EOD;
 
 
-    return ;
-
-
-
+    return $map;
 }
 
-
-
-do_shortcode('gmaps','gmaps_display');
+add_shortcode('gmaps', 'gmaps_display');
